448209b9a14030fc0b78ab88d1e71c4d4fe66291
Yahya305 <saimyahya47@gmail.com>
2024-02-06 17:59:36 +0500

    worked on basic camera setup

--- clientside/src/App.js
+++ clientside/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState,useRef } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isSwitchedOn, toggleCamera] = useState(false);
+    const cameraRef=useRef()
+    const handleButtonClick = async () => {
+        try {
+            const constraints = {
+                audio: { echoCancellation: true },
+                video: {
+                    deviceId: 0,
+                    width: { min: 200 },
+                    height: { min: 100 },
+                },
+            };
+            const stream = await navigator.mediaDevices.getUserMedia(
+                constraints
+            );
+            toggleCamera(true);
+            cameraRef.current.srcObject=stream
+            console.log("Got MediaStream:", stream);
+        } catch (error) {
+            console.error("Error accessing media devices.", error);
+        }
+    };
+    return (
+        <div className="App">
+            <h1>Lobby Page</h1>
+            <button onClick={handleButtonClick}>Open Camera</button>
+            {/* {isSwitchedOn && (
+              )} */}
+              <video ref={cameraRef} id="localVideo" autoPlay playsInline controls={false} />
+        </div>
+    );
 }
 
 export default App;
